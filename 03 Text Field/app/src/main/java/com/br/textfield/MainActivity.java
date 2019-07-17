560edd92c5bc8ededdf2f5d381a0826dc2e2be59
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-07-17 15:22:57 -0300

    Required Field

--- 03 Text Field/app/src/main/java/com/br/textfield/MainActivity.java
+++ 03 Text Field/app/src/main/java/com/br/textfield/MainActivity.java
@@ -9,11 +9,14 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     AppCompatEditText user;
     AppCompatEditText password;
     private RelativeLayout relativeLayout;
+    private TextInputLayout userLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,34 +24,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         user = findViewById(R.id.username_textfield);
+        userLayout = findViewById(R.id.username_textInputLayout);
         password = findViewById(R.id.password_textfield);
         relativeLayout = findViewById(R.id.activity_main);
 
         relativeLayout.setOnClickListener(null); //remove o focus dos campos clicando em qualquer lugar da activity
 
-        /*user.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        user.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) { //onFocusChange quando o campo está selecionado fica true, senão false
-                System.out.println(hasFocus);
+                if(user.getText().toString().isEmpty()) {
+                    userLayout.setErrorEnabled(true);
+                    userLayout.setError("Please entre your username");
+                } else {
+                    userLayout.setErrorEnabled(false);
+                }
             }
-        });*/
+        });
 
-        /*user.addTextChangedListener(new TextWatcher() {
+        user.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
-                System.out.println(s);
+
             }
 
             @Override
             public void onTextChanged(CharSequence s, int i, int i1, int i2) {
-
+                if(user.getText().toString().isEmpty()) {
+                    userLayout.setErrorEnabled(true);
+                    userLayout.setError("Please entre your username");
+                } else {
+                    userLayout.setErrorEnabled(false);
+                }
             }
 
             @Override
             public void afterTextChanged(Editable s) {
 
             }
-        });*/
+        });
     }
 
 
