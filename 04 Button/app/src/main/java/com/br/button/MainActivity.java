ea95e13ae4ed8c7a066cc9193d63116ca16e0f8c
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-07-18 19:06:55 -0300

    Raised Button

--- 04 Button/app/src/main/java/com/br/button/MainActivity.java
+++ 04 Button/app/src/main/java/com/br/button/MainActivity.java
@@ -1,14 +1,41 @@
 package com.br.button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatButton raisedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        raisedButton = findViewById(R.id.raised_btn);
+        raisedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Teste", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
