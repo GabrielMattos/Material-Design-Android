c7661520f6bdf2009a8e244de6053f06fbd7ad6e
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-08-26 13:16:36 -0300

    Create a Snackbar

---
+++ 08 Snackbar/Snackbar/app/src/main/java/com/example/snackbar/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Snackbar snackbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View view = findViewById(R.id.activity_main_ID);
+
+        snackbar = Snackbar.make(view,"Hello Snackbar!", Snackbar.LENGTH_INDEFINITE);
+        snackbar.setDuration(10000); //Em milisegundos - 10.000 == 10 segundos
+        snackbar.show();
+        //snackbar.dismiss(); //Esconde a Snackbar
+
+        //Snackbar.make(view, "Hello Snackbar!",Snackbar.LENGTH_SHORT).show();
+    }
+}
