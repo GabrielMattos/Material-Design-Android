66bd01a5847ce5e1cdeb5990c92efa0e686d2308
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-08-26 11:50:46 -0300

    Create a Linear Progress Dialog

--- 07 Dialogs/Circular Progress Dialog/app/src/main/java/com/example/circularprogressdialog/MainActivity.java
+++ 07 Dialogs/Circular Progress Dialog/app/src/main/java/com/example/circularprogressdialog/MainActivity.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private ProgressDialog dialog;
+    private Handler handler;
+    private Runnable runnable;
+    private Timer timer;
+    private int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +24,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         dialog = new ProgressDialog(MainActivity.this);
+        dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        dialog.setIndeterminate(false);
         dialog.setTitle("Progress Dialog");
         dialog.setMessage("Please Wait...");
         dialog.show();
+        dialog.setProgress(0);
+        dialog.setMax(100);
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                i += 5;
+                if(i <= 100) {
+                    dialog.setProgress(i);
+                } else {
+                    timer.cancel();
+                    dialog.cancel();
+                    i = 0;
+                }
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(runnable);
+            }
+        }, 1000, 300);
         //dialog.cancel();
     }
 }
