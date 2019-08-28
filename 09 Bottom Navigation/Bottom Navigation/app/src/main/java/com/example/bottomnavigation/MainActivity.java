8b2191123dd457a03c3644374df161fa87fcabcc
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-08-28 16:06:39 -0300

    Add an On-click Listener to the Bottom Navigation

--- 09 Bottom Navigation/Bottom Navigation/app/src/main/java/com/example/bottomnavigation/MainActivity.java
+++ 09 Bottom Navigation/Bottom Navigation/app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.bottomnavigation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
+    private TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottonNavigation_ID);
+        txt = findViewById(R.id.text_ID);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        switch (id) {
+
+            case R.id.recents_ID:
+                txt.setText(getResources().getText(R.string.recents));
+                return true;
+
+
+            case R.id.favorite_ID:
+                txt.setText(getResources().getText(R.string.favorites));
+                return true;
+
+            case R.id.nearby_ID:
+                txt.setText(getResources().getText(R.string.near_by));
+                return true;
+        }
+
+        return false;
     }
 }
