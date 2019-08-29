84aabb7cdc154fb7811e0b291da4a90301387d39
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-08-29 11:30:57 -0300

    Add a Circle Indicator to the Image SlideShow

--- 10 ViewPager & Image SlideShow/Image Slideshow/app/src/main/java/com/example/imageslideshow/MainActivity.java
+++ 10 ViewPager & Image SlideShow/Image Slideshow/app/src/main/java/com/example/imageslideshow/MainActivity.java
@@ -6,11 +6,15 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import me.relex.circleindicator.CircleIndicator;
+import me.relex.circleindicator.CircleIndicator3;
+
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private ViewPager viewPager;
     private SlideShowAdapter adapter;
+    private CircleIndicator indicator3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         viewPager = findViewById(R.id.viewPager_ID);
+        indicator3 = findViewById(R.id.circleIndicator_ID);
         adapter = new SlideShowAdapter(this);
         viewPager.setAdapter(adapter);
+        indicator3.setViewPager(viewPager);
     }
 }
