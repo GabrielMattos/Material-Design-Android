f8f617c37d5297369d4597351e296bb6773536e5
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-08-29 10:44:42 -0300

    Create an Image SlideShow - Part 2

--- 10 ViewPager & Image SlideShow/Image Slideshow/app/src/main/java/com/example/imageslideshow/MainActivity.java
+++ 10 ViewPager & Image SlideShow/Image Slideshow/app/src/main/java/com/example/imageslideshow/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.imageslideshow;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private ViewPager viewPager;
+    private SlideShowAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar_ID);
         setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager_ID);
+        adapter = new SlideShowAdapter(this);
+        viewPager.setAdapter(adapter); 
     }
 }
---
+++ 10 ViewPager & Image SlideShow/Image Slideshow/app/src/main/java/com/example/imageslideshow/SlideShowAdapter.java
@@ -0,0 +1,59 @@
+package com.example.imageslideshow;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SlideShowAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater inflater;
+    public int[] images = {
+            R.drawable.angrybirds,
+            R.drawable.cuttherope,
+            R.drawable.worms3,
+            R.drawable.clashofclans,
+            R.drawable.talkingtom,
+            R.drawable.asphalt8,
+            R.drawable.pou,
+            R.drawable.fruitninja,
+            R.drawable.wheresmywhater
+    };
+
+    public SlideShowAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return (view==(LinearLayout)object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.slideshow_layout, container, false);
+        ImageView img = view.findViewById(R.id.imageview_ID);
+        img.setImageResource(images[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
