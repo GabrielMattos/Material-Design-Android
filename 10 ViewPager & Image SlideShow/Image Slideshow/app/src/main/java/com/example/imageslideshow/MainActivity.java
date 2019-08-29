569fa3b9d21b4bcd8f8e2ca4f190402b111e4286
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-08-29 13:23:26 -0300

    Add a Timer to the Image SlideShow

--- 10 ViewPager & Image SlideShow/Image Slideshow/app/src/main/java/com/example/imageslideshow/MainActivity.java
+++ 10 ViewPager & Image SlideShow/Image Slideshow/app/src/main/java/com/example/imageslideshow/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import me.relex.circleindicator.CircleIndicator;
 import me.relex.circleindicator.CircleIndicator3;
@@ -15,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private SlideShowAdapter adapter;
     private CircleIndicator indicator3;
+    private Handler handler;
+    private Runnable runnable;
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +37,28 @@ public class MainActivity extends AppCompatActivity {
         adapter = new SlideShowAdapter(this);
         viewPager.setAdapter(adapter);
         indicator3.setViewPager(viewPager);
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                int i = viewPager.getCurrentItem();
+                if(i == adapter.images.length -1) {
+                    i = 0;
+                    viewPager.setCurrentItem(i, true);
+                } else {
+                    i++;
+                    viewPager.setCurrentItem(i, true);
+                }
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(runnable);
+            }
+        }, 4000, 4000);
     }
 }
