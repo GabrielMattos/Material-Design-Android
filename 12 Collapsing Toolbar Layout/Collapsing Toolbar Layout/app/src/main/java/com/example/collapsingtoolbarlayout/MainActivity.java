ab03e1f7a4108b7dfc5284098275a5bf92e75794
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-04 11:35:17 -0300

    Customize the Collapsing Toolbar Layout

--- 12 Collapsing Toolbar Layout/Collapsing Toolbar Layout/app/src/main/java/com/example/collapsingtoolbarlayout/MainActivity.java
+++ 12 Collapsing Toolbar Layout/Collapsing Toolbar Layout/app/src/main/java/com/example/collapsingtoolbarlayout/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar_ID);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 }
