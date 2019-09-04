7a7f9dedaa750013f460ed79e791233682644b67
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-04 11:46:38 -0300

    Add a Floating Action Button to Your CollapsingToolbarLayout

--- 12 Collapsing Toolbar Layout/Collapsing Toolbar Layout/app/src/main/java/com/example/collapsingtoolbarlayout/MainActivity.java
+++ 12 Collapsing Toolbar Layout/Collapsing Toolbar Layout/app/src/main/java/com/example/collapsingtoolbarlayout/MainActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar_ID);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        fab = findViewById(R.id.fab_ID);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Floating Action Button", Snackbar.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
