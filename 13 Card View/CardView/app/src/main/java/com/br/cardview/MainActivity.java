e96320b17cd7a64a6d288d18ab9f4e55b0ecfd01
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-04 23:07:17 -0300

    Displaying an Image View in Card View

--- 13 Card View/CardView/app/src/main/java/com/br/cardview/MainActivity.java
+++ 13 Card View/CardView/app/src/main/java/com/br/cardview/MainActivity.java
@@ -2,13 +2,41 @@ package com.br.cardview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialCardView cardView_1, cardView_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardView_1 = findViewById(R.id.cardview1_ID);
+        cardView_2 = findViewById(R.id.cardview2_ID);
+
+        cardView_1.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(view, "CardView 1", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        cardView_2.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(view, "CardView 2", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
