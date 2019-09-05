01cfe1b8ea39713dfb2105b6f41265673022f9ad
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-05 17:09:14 -0300

    Create a SwipeRefreshLayout

---
+++ 16 Swipe Refresh/Swipe Refresh/app/src/main/java/com/example/swiperefresh/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.swiperefresh;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = findViewById(R.id.SwipeRefresh_ID);
+        webView = findViewById(R.id.WebView_ID);
+    }
+}
