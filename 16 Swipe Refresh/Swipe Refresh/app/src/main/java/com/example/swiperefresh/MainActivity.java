2a4ef7be09cf0a2e864ac5dff48de0950a1a84be
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-06 10:22:01 -0300

    Implement SwipeRefreshLayout and Customize It

--- 16 Swipe Refresh/Swipe Refresh/app/src/main/java/com/example/swiperefresh/MainActivity.java
+++ 16 Swipe Refresh/Swipe Refresh/app/src/main/java/com/example/swiperefresh/MainActivity.java
@@ -23,20 +23,30 @@ public class MainActivity extends AppCompatActivity {
 
         webView.loadUrl("https://www.google.com/");
 
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+            }
+        });
+
         webViewClient = new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
+                swipeRefreshLayout.setRefreshing(false);
             }
-            
+
             @Override
             public void onPageCommitVisible(WebView view, String url) {
                 super.onPageCommitVisible(view, url);
+                swipeRefreshLayout.setColorSchemeResources(R.color.colorFinishRefresh);
             }
 
             @Override
             public void onLoadResource(WebView view, String url) {
                 super.onLoadResource(view, url);
+                swipeRefreshLayout.setColorSchemeResources(R.color.colorLoadRefresh);
             }
         };
 
