b75235a5e80608c56356a06c2f8b2106d144c79e
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-06 11:32:55 -0300

    Control RangeBar via Java Code

--- 15 Sliders/Sliders/app/src/main/java/com/example/sliders/MainActivity.java
+++ 15 Sliders/Sliders/app/src/main/java/com/example/sliders/MainActivity.java
@@ -7,12 +7,16 @@ import android.os.Bundle;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.appyvet.materialrangebar.RangeBar;
+
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppCompatSeekBar appCompatSeekBar;
     private TextView txtSeekBarValue;
+    private TextView txtRangeBarValue;
+    private RangeBar rangeBar1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         appCompatSeekBar = findViewById(R.id.SeekBar_ID);
         txtSeekBarValue = findViewById(R.id.AppCompatSeekBarValue_ID);
+        rangeBar1 = findViewById(R.id.RangeBar1_ID);
+        txtRangeBarValue = findViewById(R.id.RangeBar2TXT_ID);
 
         appCompatSeekBar.setProgress(0);
         appCompatSeekBar.setKeyProgressIncrement(1);
@@ -38,6 +44,30 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        rangeBar1.setSeekPinByIndex(0);
+        rangeBar1.setPinTextColor(getResources().getColor(R.color.colorAccent));
+        rangeBar1.setSelectorColor(getResources().getColor(R.color.colorAccent));
+        rangeBar1.setTickColors(getResources().getColor(R.color.colorAccent));
+        rangeBar1.setConnectingLineColor(getResources().getColor(R.color.colorAccent));
+        rangeBar1.setBarColor(getResources().getColor(R.color.colorAccent));
+        rangeBar1.setPinColor(getResources().getColor(R.color.colorAccent));
+        rangeBar1.setOnRangeBarChangeListener(new RangeBar.OnRangeBarChangeListener() {
+            @Override
+            public void onRangeChangeListener(RangeBar rangeBar, int leftPinIndex, int rightPinIndex, String leftPinValue, String rightPinValue) {
+                txtRangeBarValue.setText(rightPinValue);
+            }
+
+            @Override
+            public void onTouchStarted(RangeBar rangeBar) {
+
+            }
+
+            @Override
+            public void onTouchEnded(RangeBar rangeBar) {
 
             }
         });
