350fd6c0f15e6c0b3c3c8dee9bed177c17b5487c
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2019-09-06 10:58:27 -0300

    Control AppCompatSeekBar Programmatically

--- 15 Sliders/Sliders/app/src/main/java/com/example/sliders/MainActivity.java
+++ 15 Sliders/Sliders/app/src/main/java/com/example/sliders/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.sliders;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSeekBar;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatSeekBar appCompatSeekBar;
+    private TextView txtSeekBarValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appCompatSeekBar = findViewById(R.id.SeekBar_ID);
+        txtSeekBarValue = findViewById(R.id.AppCompatSeekBarValue_ID);
+
+        appCompatSeekBar.setProgress(0);
+        appCompatSeekBar.setKeyProgressIncrement(1);
+        appCompatSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                String value = String.valueOf(i);
+                txtSeekBarValue.setText(value);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
